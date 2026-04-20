d4974ed47b1d27ab242ca54d4720ebee54be0f2c
Ianbrogan2 <ian.alexander.brogan@icloud.com>
2026-04-20 12:16:39 -0700

    Update 11-CALENDAR.js

--- EDIT/11-CALENDAR.js
+++ EDIT/11-CALENDAR.js
@@ -40,7 +40,7 @@
 
 var ENN_CALENDAR = {
 
-  googleCalendarId: '',
+  googleCalendarId: '47edd2e5ee3df3afc3d3f0f0780b4b66edd0a514fe14c62adebcd56fbdb0f469@group.calendar.google.com',
   // ↑ Paste your Google Calendar ID here (see setup instructions above)
 
   // Color legend shown below the calendar on the site.
